eb6dcc42e007ddcff3507aaa3fc56525d8f45513
waynewec <waynewec@gmail.com>
2024-07-28 11:59:27 -0600

    Working computer again

--- keyboards/kbdfans/kbd75rgb/keymaps/waynewec/keymap.c
+++ keyboards/kbdfans/kbd75rgb/keymaps/waynewec/keymap.c
@@ -14,20 +14,23 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "host.h"
+#include "quantum.h"
+#include "rgb_matrix.h"
 #include QMK_KEYBOARD_H
 
-enum my_keycodes {
-    FOO = SAFE_RANGE,
-    BAR
-};
+/* enum my_keycodes { */
+/*     FOO = SAFE_RANGE, */
+/*     BAR */
+/* }; */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
+    // clang-format off
   [0] = LAYOUT_75_ansi(
     KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,  KC_INS, KC_DEL,
     KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_HOME,
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_PGUP,
-    KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,   KC_PGDN,
+    MT(MOD_LCTL,KC_ESC),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,   KC_PGDN,
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
     KC_LCTL,  KC_LGUI,  KC_LALT,                               KC_SPC,              KC_RALT,            MO(1),    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
@@ -59,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 
 };
-
+//clang-format on
 bool rgb_matrix_indicators_user(void) {
     if (host_keyboard_led_state().caps_lock) {
         rgb_matrix_set_color(59, RGB_BLUE);
